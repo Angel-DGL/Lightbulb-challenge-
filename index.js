a8316008851a4ba4bcf4878bd7b25e4dbce53148
Kaneki08 <derickalexander98@gmail.com>
2023-07-03 22:28:27 -0700

    lightbulb

---
+++ index.js
@@ -0,0 +1,7 @@
+var img = document.getElementById ("lightbulb");
+    function off(){
+           document.getElementById('chesburger').src="https://as2.ftcdn.net/v2/jpg/00/38/99/99/1000_F_38999921_dagMVem5FrDAgCJW4iU4lFhFmD44m2MG.jpg";
+    } 
+function on(){
+        document.getElementById('chesburger').src="https://www.hometownevolutioninc.com/wp-content/uploads/2019/04/A15_clear_light_bulbs_E26_commercial.jpg";
+}
